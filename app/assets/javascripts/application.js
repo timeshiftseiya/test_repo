247f66dad4139d3a2cb4121308eba99f2f6e7d9f
kiyofumi ito <sodapoptky@gmail.com>
2018-06-22 11:27:34 +0000

    js読み込み

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery.turbolinks
-//= require jquery_ujs
-//= require twitter/bootstrap
+//= require rails-ujs
 //= require turbolinks
+//= require jquery/dist/jquery.js
+//= require bootstrap/dist/js/bootstrap.min
 //= require_tree .
