f90d16a8daa6e7eceb4dfc466ba09e825fdf1615
kiyofumi ito <sodapoptky@gmail.com>
2018-06-22 11:57:47 +0000

    js読み込み

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,6 @@
 //= require turbolinks
 //= require jquery/dist/jquery.js
 //= require bootstrap/dist/js/bootstrap.min
+//= require rails-ujs
+//= require turbolinks
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,11 @@ Rails.application.routes.draw do
 
   devise_for :users
   
+  resources :users, only: [:index,:show]
+  
   devise_scope :user do
     root :to => "devise/sessions#new"
   end
-  
-  resources :users, only: [:index,:show] 
-  
   resources :relationships, only: [:create, :destroy]
    
    
