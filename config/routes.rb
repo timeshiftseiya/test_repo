59c6074ea56e79ac2edc817f6e44976b2da6b400
kiyofumi ito <sodapoptky@gmail.com>
2018-06-23 12:20:24 +0000

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,22 @@
 Rails.application.routes.draw do
   
-  
-
-  devise_for :users
-  
-  resources :users, only: [:index,:show]
-  
-  devise_scope :user do
-    root :to => "devise/sessions#new"
-  end
-  
- 
-  
-  resources :relationships, only: [:create, :destroy]
-   
-   
-   
   resources :wines do
     resources :comments
       collection do
         post :confirm
       end
   end
+  
+  devise_scope :user do
+    root :to => "devise/sessions#new"
+    get 'logout', to: "devise/sessions#destroy", as: "logout"
+  end
+
+  devise_for :users
+  
+  resources :users, only: [:index,:show]
+  
+  resources :relationships, only: [:create, :destroy]
   
   resources :favorites, only: [:create, :destroy, :index]
   
