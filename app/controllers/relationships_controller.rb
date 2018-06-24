95d3faad347ab738124f91fa1f96affab77b0b7d
kiyofumi ito <sodapoptky@gmail.com>
2018-06-24 04:48:07 +0000

    respondの記述を変更

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,17 +1,25 @@
 class RelationshipsController < ApplicationController
   before_action :authenticate_user!
   protect_from_forgery except: [:create, :destroy]
-  respond_to :js
+  # respond_to :js
 
   def create
-    @user = User.find(params[:relationship][:followed_id])
-    current_user.follow!(@user)
-    respond_with @user
+    respond_to do |format|
+      format.js do
+        @user = User.find(params[:relationship][:followed_id])
+        current_user.follow!(@user)
+      end
+    end
+    # respond_with @user
   end
   
   def destroy
-    @user = Relationship.find(params[:id]).followed
-    current_user.unfollow!(@user)
-    respond_with @user
+    respond_to do |format|
+      format.js do
+        @user = Relationship.find(params[:id]).followed
+        current_user.unfollow!(@user)
+      end  
+    end
+    # respond_with @user
   end
 end
