742859be45d617c86c64525128fff042b5536b3c
kiyofumi ito <sodapoptky@gmail.com>
2018-06-24 04:52:35 +0000

    form_forをwithに変更

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,25 +1,17 @@
 class RelationshipsController < ApplicationController
   before_action :authenticate_user!
   protect_from_forgery except: [:create, :destroy]
-  # respond_to :js
+  respond_to :js
 
   def create
-    respond_to do |format|
-      format.js do
-        @user = User.find(params[:relationship][:followed_id])
-        current_user.follow!(@user)
-      end
-    end
-    # respond_with @user
+    @user = User.find(params[:relationship][:followed_id])
+    current_user.follow!(@user)
+    respond_with @user
   end
   
   def destroy
-    respond_to do |format|
-      format.js do
-        @user = Relationship.find(params[:id]).followed
-        current_user.unfollow!(@user)
-      end  
-    end
-    # respond_with @user
+    @user = Relationship.find(params[:id]).followed
+    current_user.unfollow!(@user)
+    respond_with @user
   end
 end
