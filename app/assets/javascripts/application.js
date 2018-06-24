096f6eb68b1961702a823b06d0d3d0500c154ab3
kiyofumi ito <sodapoptky@gmail.com>
2018-06-24 04:56:55 +0000

    compileのし直しとjquery-ujs導入

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,7 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery_ujs
 //= require rails-ujs
 //= require jquery/dist/jquery.js
 //= require bootstrap/dist/js/bootstrap.min
