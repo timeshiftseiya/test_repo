cb2425be97972e002625fc1e5352801cc7d2e99f
seiyamiyaoka <seiyamiyaoka@gmail.com>
2018-06-24 20:48:15 +0900

    ffff

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,17 +4,13 @@ class RelationshipsController < ApplicationController
   def create
     @user = User.find(params[:relationship][:followed_id])
     current_user.follow!(@user)
-    respond_to do |format|
-      format.js { render :create }
-    end
+    
     
   end
   
   def destroy
     @user = Relationship.find(params[:id]).followed
     current_user.unfollow!(@user)
-    respond_to do |format|
-      format.js { render :destroy }
-    end
+    
   end
 end
