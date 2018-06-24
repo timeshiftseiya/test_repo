0fb8cd909a6161871287817d9c737067ae18f5bf
kiyofumi ito <sodapoptky@gmail.com>
2018-06-24 02:42:10 +0000

    recompile

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   
   resources :wines do
     resources :comments
@@ -12,7 +13,7 @@ Rails.application.routes.draw do
     get 'logout', to: "devise/sessions#destroy", as: "logout"
   end
 
-  devise_for :users
+  
   
   resources :users, only: [:index,:show]
   
