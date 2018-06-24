7b0cab8504574625245f0111465b369997cf16ee
kiyofumi ito <sodapoptky@gmail.com>
2018-06-24 04:35:55 +0000

    再度元に戻す

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,25 +1,17 @@
 class RelationshipsController < ApplicationController
   before_action :authenticate_user!
   protect_from_forgery except: [:create, :destroy]
-  # respond_to :js
+  respond_to :js
 
   def create
     @user = User.find(params[:relationship][:followed_id])
     current_user.follow!(@user)
-    @user
-    respond_to do |format|
-      format.js
-    end  
-    # respond_with @user
+    respond_with @user
   end
   
   def destroy
     @user = Relationship.find(params[:id]).followed
     current_user.unfollow!(@user)
-    @user
-    respond_to do |format|
-      format.js
-    end  
-    # respond_with @user
+    respond_with @user
   end
 end
