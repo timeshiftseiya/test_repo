46329e99ab498ad5771240a80a567b5ff7554081
seiyamiyaoka <seiyamiyaoka@gmail.com>
2018-06-24 20:19:25 +0900

    ffff

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,23 +1,20 @@
 class RelationshipsController < ApplicationController
   before_action :authenticate_user!
-  #protect_from_forgery except: [:create, :destroy]
   
   def create
     @user = User.find(params[:relationship][:followed_id])
     current_user.follow!(@user)
-    # respond_to do |format|
-    #   format.js {render :create}
-    # endi
-    redirect_to root_path
+    respond_to do |format|
+      format.js { render :create }
+    end
     
   end
   
   def destroy
     @user = Relationship.find(params[:id]).followed
     current_user.unfollow!(@user)
-    # respond_to do |format|
-    #   format.js {render :destroy}
-    # end 
-    redirect_to root_path
+    respond_to do |format|
+      format.js { render :destroy }
+    end
   end
 end
